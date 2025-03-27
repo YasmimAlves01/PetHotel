4c586b0e9439e5f94738bf68f0d0523b2e69fc96
Yasumii007 <yasmimalvesrodrigues16@gmail.com>
2025-03-26 21:51:11 -0300

    Atividade pet hotel, que gera uma ficha para um aniaml de stimação de acordo com as informações dadas pelo usuario

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿string especie, raca, alcunha, pelagem;
+int idade;
+
+Console.WriteLine("Bem-vindo ao pet Hotel Dog Small");
+
+Console.WriteLine("Por favor digite a especie: ");
+especie = Console.ReadLine()!;
+
+Console.WriteLine("Por favor digite a raça do seu animal:");
+raca = Console.ReadLine()!;
+
+Console.WriteLine("Por favor digite o alcunha do seu pet:");
+alcunha = Console.ReadLine()!;
+
+Console.WriteLine("Por favor digite a idade do seu pet:");
+while (!int.TryParse(Console.ReadLine(), out idade) || idade < 0)
+{
+    Console.Write("idade não valida. Digite novamente: ");
+}
+
+Console.WriteLine("Por favor digite a pelagem do seu cão:");
+pelagem = Console.ReadLine()!;
+
+Console.WriteLine("+=========================================================+");
+Console.WriteLine("|                 Pet Hotel \"Dog Small\"                  |");
+Console.WriteLine("+=========================================================+");
+Console.WriteLine($"| Espécie: {especie.PadLeft(18,'.')} | Raça: {raca.PadLeft(19,'.')} |");
+Console.WriteLine("+=========================================================+");
+Console.WriteLine($"| Atende pela alcunha de: {alcunha.PadLeft(31,'.')} |");
+Console.WriteLine($"| Idade: {idade.ToString().PadLeft(2,'0') } ano(s) | Pelagem/cor: {pelagem.PadLeft(23,'.')} |");
+Console.WriteLine("+=========================================================+");
