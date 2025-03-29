1fb79010c8789882896617ba5c5d46088980b760
Yasumii007 <yasmimalvesrodrigues16@gmail.com>
2025-03-28 23:25:10 -0300

    colocando corzinhas

--- Program.cs
+++ Program.cs
@@ -1,31 +1,70 @@
 ﻿string especie, raca, alcunha, pelagem;
-int idade;
+        int idade;
 
-Console.WriteLine("Bem-vindo ao pet Hotel Dog Small");
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine("Bem-vindo ao Pet Hotel Dog Small");
+        Console.ResetColor();
 
-Console.WriteLine("Por favor digite a especie: ");
-especie = Console.ReadLine()!;
+        Console.Write("Por favor, digite a espécie: ");
+        especie = Console.ReadLine()!;
 
-Console.WriteLine("Por favor digite a raça do seu animal:");
-raca = Console.ReadLine()!;
+        Console.Write("Por favor, digite a raça do seu animal: ");
+        raca = Console.ReadLine()!;
 
-Console.WriteLine("Por favor digite o alcunha do seu pet:");
-alcunha = Console.ReadLine()!;
+        Console.Write("Por favor, digite a alcunha do seu pet: ");
+        alcunha = Console.ReadLine()!;
 
-Console.WriteLine("Por favor digite a idade do seu pet:");
-while (!int.TryParse(Console.ReadLine(), out idade) || idade < 0)
-{
-    Console.Write("idade não valida. Digite novamente: ");
-}
+        Console.Write("Por favor, digite a idade do seu pet: ");
+        while (!int.TryParse(Console.ReadLine(), out idade) || idade < 0)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("Erro! Digite novamente: ");
+            Console.ResetColor();
+        }
 
-Console.WriteLine("Por favor digite a pelagem do seu cão:");
-pelagem = Console.ReadLine()!;
+        Console.Write("Por favor, digite a pelagem do seu animal: ");
+        pelagem = Console.ReadLine()!;
 
-Console.WriteLine("+=========================================================+");
-Console.WriteLine("|                 Pet Hotel \"Dog Small\"                  |");
-Console.WriteLine("+=========================================================+");
-Console.WriteLine($"| Espécie: {especie.PadLeft(18,'.')} | Raça: {raca.PadLeft(19,'.')} |");
-Console.WriteLine("+=========================================================+");
-Console.WriteLine($"| Atende pela alcunha de: {alcunha.PadLeft(31,'.')} |");
-Console.WriteLine($"| Idade: {idade.ToString().PadLeft(2,'0') } ano(s) | Pelagem/cor: {pelagem.PadLeft(23,'.')} |");
-Console.WriteLine("+=========================================================+");
+        Console.Clear();
+
+        Console.ForegroundColor = ConsoleColor.Gray;
+        Console.WriteLine("+=========================================================+");
+        Console.Write("|");    
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.Write("                 Pet Hotel \"Hotel Dog Small\"             ");
+        Console.ForegroundColor = ConsoleColor.Gray;
+        Console.WriteLine("|");  
+        Console.WriteLine("+=========================================================+");
+
+        Console.Write("|"); 
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.Write($" Espécie: ");
+         Console.ForegroundColor = ConsoleColor.Gray;
+        Console.Write($"{especie.PadLeft(18,'.')} ");
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.Write("| Raça: ");
+        Console.ForegroundColor = ConsoleColor.Gray;
+        Console.Write($"{raca.PadLeft(19,'.')} ");
+        Console.WriteLine("|"); 
+        Console.WriteLine("+=========================================================+");
+        
+        Console.Write("|");
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.Write(" Atende pela alcunha de: ");
+        Console.ForegroundColor = ConsoleColor.Gray;
+        Console.Write($"{alcunha.PadLeft(31,'.')} ");
+        Console.WriteLine("|");
+
+        Console.Write("|");
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.Write(" Idade: ");
+        Console.ForegroundColor = ConsoleColor.Gray;
+        Console.Write($"{idade.ToString().PadLeft(2,'0')} ano(s)");
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.Write(" | Pelagem/cor: ");
+        Console.ForegroundColor = ConsoleColor.Gray;
+        Console.Write($"{pelagem.PadLeft(23,'.')} ");
+        Console.WriteLine("|");
+
+        Console.WriteLine("+=========================================================+");
+        Console.ResetColor();
